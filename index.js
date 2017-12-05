6d954bc2e9c0cefa289d1b5ee512f282a62b3a90
Zoe Lee <zoe@kihira.uk>
2017-12-05 22:45:27 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const discord_js_1 = require("discord.js");
+const client = new discord_js_1.Client();
+client.on("ready", () => {
+    console.log("Ready!");
+});
+client.login("Mzg3Njc0OTgwMDEzNTcyMDk3.DQif1Q.dXSar0OTWb2j4FkP3DEljMf0Peg");
