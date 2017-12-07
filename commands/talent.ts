8310bdf3b6e2a25745ad91b44f6f8d681acec6c5
Zoe <zoe.kihira.lee@gmail.com>
2017-12-07 22:43:39 +0100

    escape regex for talent input

--- commands/talent.ts
+++ commands/talent.ts
@@ -1,11 +1,12 @@
 import {Message} from "discord.js";
 import {Mongo} from "../db";
+import {escapeRegex} from "../util";
 import {Command} from "./command";
 
 export class Talent extends Command {
     public async run(message: Message): Promise<void> {
         if (message.content.length > 7) {
-            const talent = message.content.replace(" ", "_").substr(8);
+            const talent = escapeRegex(message.content.replace(/ /g, "_").substr(8));
             const data = await Mongo.Data.collection("talents").findOne({_id: {$regex: talent, $options: "i"}});
             if (data == null) {
                 await message.reply("No talent found");
