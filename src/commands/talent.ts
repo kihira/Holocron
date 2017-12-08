881dddb9c814b555517ca5899ea8168c872f78de
Zoe Lee <zoe@kihira.uk>
2017-12-08 17:27:33 +0100

    Add func to convert id to name and back and add title to embed

--- src/commands/talent.ts
+++ src/commands/talent.ts
@@ -1,12 +1,12 @@
 import {Message, RichEmbedOptions} from "discord.js";
 import {Mongo} from "../db";
-import {escapeRegex} from "../util";
+import {escapeRegex, idToName, nameToId} from "../util";
 import {Command} from "./command";
 
 export class Talent extends Command {
     public async run(message: Message): Promise<void> {
         if (message.content.length > 7) {
-            const talent = escapeRegex(message.content.replace(/ /g, "_").substr(8));
+            const talent = escapeRegex(nameToId(message.content.substr(8))); // todo substr needs support for alias
             const data = await Mongo.Data.collection("talents").findOne({_id: {$regex: talent, $options: "i"}});
             if (data == null) {
                 await message.reply("No talent found");
@@ -28,6 +28,7 @@ export class Talent extends Command {
                             value: data.force ? "True" : "False",
                         },
                     ],
+                    title: idToName(data._id),
                 };
                 if (process.env.DATA_URL !== undefined) {
                     embed.url = process.env.DATA_URL + "/talents/" + data._id;
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,11 @@
 export function escapeRegex(str: string) {
     return str.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&");
 }
+
+export function idToName(id: string) {
+    return id.replace(/_/g, " ");
+}
+
+export function nameToId(name: string) {
+    return name.replace(/ /g, "_");
+}
