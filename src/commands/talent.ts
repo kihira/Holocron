304e410bcb3809ecd326309fe76ce7f78aa1bc2d
Zoe Lee <zoe@kihira.uk>
2017-12-09 17:23:57 +0100

    Improve talent output

--- src/commands/talent.ts
+++ src/commands/talent.ts
@@ -1,39 +1,57 @@
-import {Message, RichEmbedOptions} from "discord.js";
+import {Message, RichEmbed} from "discord.js";
+import {isObject} from "util";
 import {Mongo} from "../db";
 import {escapeRegex, idToName, nameToId} from "../util";
 import {Command} from "./command";
+
+interface ITalent {
+    _id: string;
+    force: boolean;
+    ranked: boolean;
+    index: string[];
+    short?: string;
+    description?: string;
+    activation: boolean | {Action?: boolean, Incidental?: boolean, Out_Of_Turn?: boolean};
+}
 
 export class Talent extends Command {
     public async run(message: Message): Promise<void> {
         if (message.content.length > 7) {
             const talent = escapeRegex(nameToId(message.content.substr(8))); // todo substr needs support for alias
-            const data = await Mongo.Data.collection("talents").findOne({_id: {$regex: talent, $options: "i"}});
+            const data = await Mongo.Data.collection<ITalent>("talents")
+                .findOne({_id: {$regex: talent, $options: "i"}});
 
             if (data == null) {
                 await message.reply("No talent found");
                 return;
             }
 
-            const embed: RichEmbedOptions = {
-                author: {
-                    icon_url: message.member.user.avatarURL,
-                    name: message.member.displayName,
-                },
-                description: data.description || data.short,
-                fields: [
-                    {
-                        name: "Ranked",
-                        value: data.ranked ? "True" : "False",
-                    },
-                    {
-                        name: "Force",
-                        value: data.force ? "True" : "False",
-                    },
-                ],
-                title: idToName(data._id),
-            };
+            const embed = new RichEmbed();
+            embed.setTitle(idToName(data._id));
+            embed.setAuthor(message.member.displayName, message.member.user.avatarURL);
+            embed.setDescription(data.description || data.short);
+            embed.setFooter(data.index.join(", "));
+            embed.addField("Ranked", data.ranked ? "True" : "False", true);
+            embed.addField("Force", data.force ? "True" : "False", true);
+
+            if (!data.activation) {
+                embed.addField("Activation", "Passive", true);
+            }
+            else {
+                let value = "Active";
+                if (isObject(data.activation)) {
+                    value += " (";
+                    const keys = Object.keys(data.activation);
+                    for (let i = 0; i < keys.length; i++) {
+                        keys[i] = idToName(keys[i]);
+                    }
+                    value += keys.join(", ");
+                    value += ")";
+                }
+                embed.addField("Activation", value, true);
+            }
             if (process.env.DATA_URL !== undefined) {
-                embed.url = process.env.DATA_URL + "/talents/" + data._id;
+                embed.setURL(process.env.DATA_URL + "/talents/" + data._id);
             }
             await message.channel.send({embed});
         }
