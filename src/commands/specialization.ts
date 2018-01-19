ee2603b053be48524da4a52d280310e8d4b95830
Zoe Lee <zoe@kihira.uk>
2018-01-19 17:04:38 +0100

    Add in specialization command

---
+++ src/commands/specialization.ts
@@ -0,0 +1,40 @@
+import {Message, RichEmbed} from "discord.js";
+import {Database, Entry} from "../db";
+import {escapeRegex, idToName, nameToId} from "../util";
+import {Argument, Command} from "./command";
+
+interface ISpecialization extends Entry {
+    _id: string;
+    skills: string[];
+    tree: {talents: string[][]};
+}
+
+export = class Armor extends Command {
+    constructor() {
+        super("specialization", [new Argument("name")], "specialisation", "spec");
+    }
+    public async run(message: Message, args: string[]): Promise<void> {
+        const talent = escapeRegex(nameToId(args[0]));
+        const data = await Database.Data.collection<ISpecialization>("specializations")
+            .find({name: {$regex: talent, $options: "i"}}).limit(1).next();
+
+        if (data == null) {
+            await message.channel.send("No armor found");
+            return;
+        }
+
+        const embed = new RichEmbed();
+        embed.setTitle(idToName(data._id));
+        embed.setAuthor(message.member.displayName, message.author.avatarURL);
+        embed.setDescription(data.description || "");
+        embed.setFooter(data.index.join(", "));
+        embed.setColor("DARK_RED");
+        embed.addField("Skills", data.skills.map((value) => idToName(value)).join(" ,"));
+        // embed.addField("Tree", data.tree); // todo
+        if (process.env.DATA_URL !== undefined) {
+            embed.setURL(process.env.DATA_URL + "/specializations/" + data._id);
+        }
+
+        await message.channel.send(embed);
+    }
+};
